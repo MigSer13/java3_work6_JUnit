1a0fd393d334588d8f7a4f648b4192c19f10a590
MigSer13 <avto470@mail.ru>
2021-04-04 16:21:40 +0300

    2

--- src/main/java/Task3.java
+++ src/main/java/Task3.java
@@ -3,9 +3,11 @@ public class Task3 {
     public boolean checkingArrayFor_1_and_4(int[] array){
         for (int i = 0; i < array.length; i++) {
             if(array[i] == 1 || array[i] == 4 ){
+                System.out.println(true);
                 return true;
             }
         }
+        System.out.println(false);
         return false;
     }
 }
